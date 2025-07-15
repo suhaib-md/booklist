100627879ba12ef72233d7c1f523434e977c87e2
Muhammed Suhaib <suhaib.muhammed2002@gmail.com>
2025-07-15 10:12:42 +0000

    GenkitError: FAILED_PRECONDITION: Please pass in the API key or set the

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,15 @@
+'use server';
+import { config } from 'dotenv';
+config();
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
